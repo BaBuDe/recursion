ed508cd9f88f2abaf9506cb16e3c367b95585310
C. Moss <caly.moss@gmail.com>
2014-05-20 12:50:17 -0700

    Initial pseudo code for solution

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,38 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+	if (typeof obj === 'object') {
+		_.each(obj, function () {
+			if (typeof value === 'function' || XML || undefined) {
+				delete value;
+		});
+	};
+	else if (Array.isArray(obj)) {
+		_.each(obj, function(value, index) {
+			if (typeof value === 'function' || XML || undefined) {
+				value = null;
+		});
+	};
+	else if (typeof obj = 'Boolean' || Number || 'string') {
+		//convert to primitive value w/traditional conversion semantics...whatever that means;
+	};
 };
+
+//So essentially what I am doing is re-creating the replacer function instead of passing that in as an argument.
+
+//Properties of non-array objects are not guaranteed to be stringified in any particular order. 
+//Do not rely on ordering of properties within the same object within the stringification.
+//Boolean, Number, and String objects are converted to the corresponding primitive values 
+//during stringification, in accord with the traditional conversion semantics.
+//If undefined, a function, or an XML value is encountered during conversion it is either omitted 
+//(when it is found in an object) or censored to null (when it is found in an array).
+
+//******Replacer function:
+//can be function or array
+//If a function, transforms values and properties encountered while stringifying; 
+//if an array, specifies the set of properties included in objects in the final string.
+//A detailed description of the replacer function is provided in the javaScript guide article Using native JSON.
+
+
+-So, object properties end up randomly ordered
+-
