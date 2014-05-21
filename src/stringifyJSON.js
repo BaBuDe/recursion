90b4c7ffb70eebbea7a0b9a488e532fa753093c8
C. Moss <caly.moss@gmail.com>
2014-05-20 20:35:37 -0700

    base case added

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,48 +1,56 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
+// // this is what you would do if you liked things to be easy:
+// // var stringifyJSON = JSON.stringify;
 
-// but you don't so you're going to write it from scratch:
+// // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-	if (Array.isArray(obj)) {
-	 	var jObj = {};
-	 };
-    for (var i in obj) {
-       jObj[i] = obj[i];
-    };
-	
-	
-	_.each(obj, function(value) {
-		if (typeof value === 'object') {
-			return stringifyJSON(value);
-		}
-		else if (Array.isArray(value)) {
-			_.each(value, function(item) {
-				return stringifyJSON(item);
-			});
-		};
-		else if (typeof item === 'function' || or item === undefined) {
-			delete item || item = null; //how to properly omit? and how to determine the path (was it in a function or an object?)?
-		};
-		else value = value.toString();
-	});
-	
-	return obj.toString();
+
+	if (obj === null) {
+		return "null";
+	}
+	else if (obj === undefined) {
+		return "undefined";
+	}
+	else if (typeof obj === 'boolean' || typeof obj === 'number' || typeof obj === 'string') {
+		return '"' + obj + '"';
+	}
+	else if (Array.isArray(obj)) {
+		_.each(obj, function(value) {
+			if (value === undefined || typeof value === 'function') {
+				return null;
+			} else stringifyJSON(value);
+		});
+		return '"' + obj + '"';
+	};
+	else if (typeof obj === 'object') {
+		 _.each(obj, function(value) {
+			if (value === undefined || typeof value === 'function') {
+				return "";
+			} else stringifyJSON(value);
+		});
+		return '"' + obj + '"';
+	};
+
+
+
 };
 
 
-//So essentially what I am doing is re-creating the replacer function instead of passing that in as an argument.
 
-//Properties of non-array objects are not guaranteed to be stringified in any particular order. 
-//Do not rely on ordering of properties within the same object within the stringification.
-//Boolean, Number, and String objects are converted to the corresponding primitive values 
-//during stringification, in accord with the traditional conversion semantics.
-//If undefined, a function, or an XML value is encountered during conversion it is either omitted 
-//(when it is found in an object) or censored to null (when it is found in an array).
+// //So essentially what I am doing is re-creating the replacer function instead of passing that in as an argument.
 
-//******Replacer function:
-//can be array OR function
-//If a function, transforms values and properties encountered while stringifying; 
-//if an array, specifies the set of properties included in objects in the final string.
-//A detailed description of the replacer function is provided in the javaScript guide article Using native JSON.
+// //Properties of non-array objects are not guaranteed to be stringified in any particular order. 
+// //Do not rely on ordering of properties within the same object within the stringification.
+// //Boolean, Number, and String objects are converted to the corresponding primitive values 
+// //during stringification, in accord with the traditional conversion semantics.
+// //If undefined, a function, or an XML value is encountered during conversion it is either omitted 
+// //(when it is found in an object) or censored to null (when it is found in an array).
 
-//probably don't want _.map because it returns an array
+// //******Replacer function:
+// //can be array OR function
+// //If a function, transforms values and properties encountered while stringifying; 
+// //if an array, specifies the set of properties included in objects in the final string.
+// //A detailed description of the replacer function is provided in the javaScript guide article Using native JSON.
+
+// //probably don't want _.map because it returns an array
+
+
