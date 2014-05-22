089464c263ca496cd23ebb8236e829871367a2c8
C. Moss <caly.moss@gmail.com>
2014-05-21 21:07:28 -0700

    Initial non-workingg solution

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,44 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+//should this be in here? how do you pass the root node to the function???
+    var root = document.body;
+
+    $('body').addClass(className);
+
+
+
+		var matchingElements = [];
+
+		var crawlDOM = function(node, func) {
+		    func(node);
+		    node = node.firstChild;
+		    while (node) {
+		        crawlDOM(node, func);
+		        node = node.nextSibling;
+		    }
+		};
+
+		crawlDOM(root, function (className, node) {
+			if (node.classList.contains(className)) {
+				matchingElements.push(node);
+			}
+		});
+
+  
+    return matchingElements;
+
 };
+
+//returns array-like object with all child nodes w/that class name
+//when called on DOM or document, searches everything including root node
+//otherwise searches descendents of root element called on
+
+//document.body
+//element.childNodes
+//element.classList
+
+//--the element being passed in is not the element being recursed on
+//use recursion to walk through all the descendent nodes?
+//nested approach vs not
+
